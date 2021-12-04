4f05c12a2c3d8e8834a2b5d7ad250e8ce1564ade
Emre Kök <38594941+emrekokk@users.noreply.github.com>
2021-12-05 01:57:45 +0300

    User&Task Classes Created
    -User class created
    -Task class created
    -Some UI changes to show task on screen

---
+++ Todo-List/Task.swift
@@ -0,0 +1,33 @@
+//
+//  Task.swift
+//  Todo-List
+//
+//  Created by Emre Kök on 5.12.2021.
+//
+
+import UIKit
+
+class Task {
+    
+    var text:String?
+    
+    var date:Date?
+    
+    var header:String?
+    
+    var status:Bool?// task finished or will do
+    //can be use enum - true finished - false 
+    
+    init(text: String,date:Date,header:String,status:Bool){
+        self.text = text
+        self.date = date
+        self.status = false
+        self.header = header
+    }
+    static func generateTaskList() -> [Task]
+    {
+        let taskList: [Task] = []
+        return taskList
+    }
+    
+}
