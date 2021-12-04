3510b31b0e52c1e1703e1f3b3777523de5ff3fda
Emre Kök <38594941+emrekokk@users.noreply.github.com>
2021-12-04 23:59:50 +0300

    Hello,world!

--- Todo-List/ViewController.swift
+++ Todo-List/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        label.text = "Hello World!"
+    }
+    
 }
 
